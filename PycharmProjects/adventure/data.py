7675d92d4f5cbeeba5621e83c3676abeb0082196
David Evans <devans6915@aol.com>
2017-03-02 18:02:51 +0000

    Add files via upload

---
+++ PycharmProjects/adventure/data.py
@@ -0,0 +1,11 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 12:39:47) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+locations = {
+    (0, 0): 'house',
+    (0, 1): 'lake',
+    (1, 0): 'park',
+    (1, 1): 'market',
+}
